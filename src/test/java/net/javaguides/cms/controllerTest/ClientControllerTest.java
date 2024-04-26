42b65250f9605924b4fc059df6fdd7bdb0cf75ec
anusham9 <148721336+anusham9@users.noreply.github.com>
2024-04-26 16:07:17 -0700

    Update ClientControllerTest.java

--- src/test/java/net/javaguides/cms/controllerTest/ClientControllerTest.java
+++ src/test/java/net/javaguides/cms/controllerTest/ClientControllerTest.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package net.javaguides.cms.controllerTest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
